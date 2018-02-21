e3f47ef3d871b97f52922e95f66a7d5484b29bc1
wuweiweiwu <wuxx1045@umn.edu>
2018-02-21 03:10:10 -0800

    Initial commit from nwb v0.21.5

---
+++ nwb.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  type: 'react-component',
+  npm: {
+    esModules: true,
+    umd: {
+      global: 'ReactShopifyDraggable',
+      externals: {
+        react: 'React'
+      }
+    }
+  }
+}
