1a4350915b78d40922408f4422b46de2d2a1847d
wuweiweiwu <wuxx1045@umn.edu>
2018-02-22 20:54:23 -0800

    Draggable and Handle

--- src/Draggable/Handle.js
+++ src/Draggable/Handle.js
@@ -1,5 +1,46 @@
-import React, { Component } from 'react';
+// @flow
+import React, { Component, type Node } from 'react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
 
-class Handle extends Component {}
+type Props = {
+  as: string, // what to render the container as
+  className?: string, // classname for the container
+  id?: string, // id for the container
+  style?: { [string]: string | number }, //inline styling
+  children?: Node,
+};
+
+class Handle extends Component<Props> {
+  static contextTypes = {
+    contextWrapper: PropTypes.object,
+  };
+
+  static defaultProps = {
+    as: 'div',
+  };
+
+  // subscribe to context updates
+  componentDidMount() {
+    this.context.contextWrapper.subscribe(() => this.forceUpdate());
+  }
+
+  render() {
+    const { as: ElementType, className, id, style, children } = this.props;
+
+    return (
+      <ElementType
+        id={id}
+        className={classNames([
+          className,
+          this.context.contextWrapper.handleClassName,
+        ])}
+        style={style}
+      >
+        {children}
+      </ElementType>
+    );
+  }
+}
 
 export default Handle;
