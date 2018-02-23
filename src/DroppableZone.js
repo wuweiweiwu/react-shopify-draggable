d02292c58dc713ee0bd874089c2eab0b9a0b5647
wuweiweiwu <wuxx1045@umn.edu>
2018-02-22 23:48:50 -0800

    DroppableZone

---
+++ src/DroppableZone.js
@@ -0,0 +1,46 @@
+// @flow
+import React, { PureComponent, type Node } from 'react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+
+type Props = {
+  as: string, // what to render the container as
+  className?: string, // classname for the container
+  id?: string, // id for the container
+  style?: { [string]: string | number }, //inline styling
+  children?: Node,
+};
+
+class DroppableZone extends PureComponent<Props> {
+  static contextTypes = {
+    contextWrapper: PropTypes.object,
+  };
+
+  static defaultProps = {
+    as: 'div',
+  };
+
+  // subscribe to context updates
+  componentDidMount() {
+    this.context.contextWrapper.subscribe(() => this.forceUpdate());
+  }
+
+  render() {
+    const { as: ElementType, className, id, style, children } = this.props;
+
+    return (
+      <ElementType
+        id={id}
+        className={classNames([
+          className,
+          this.context.contextWrapper.droppableClassName,
+        ])}
+        style={style}
+      >
+        {children}
+      </ElementType>
+    );
+  }
+}
+
+export default DroppableZone;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import DraggableItem from './DraggableItem';
 import DraggableContainer from './DraggableContainer';
 import DraggableHandle from './DraggableHandle';
+import DroppableZone from './DroppableZone';
 
 export { DraggableItem, DraggableContainer, DraggableHandle };
